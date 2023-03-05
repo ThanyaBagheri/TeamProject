fb3b43aed3ec73c26e1528f1f91aa8fecf2dc1a8
ThanyaBagheri <120802259+ThanyaBagheri@users.noreply.github.com>
2023-03-05 13:03:38 -0500

    added US303

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -1,12 +1,16 @@
 package tests;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.BasePage;
 import pages.HomePage;
+
+import java.util.List;
 
 public class HomeTest extends BaseTest{
     HomePage page;
@@ -42,5 +46,20 @@ public class HomeTest extends BaseTest{
         Assert.assertEquals(check, true);
 
 
+    }
+
+    @Test(testName = "US 303: When problem_user logs in all items on homepage should display same images")
+    public void test303() {
+        driver.findElement(By.id("user-name")).sendKeys("problem_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        List<WebElement> pictures = driver.findElements(By.xpath("//img[@class='inventory_item_img']"));
+        //pictures.forEach(each -> System.out.println(each.getAttribute("src")) );
+
+        String picturePath = "https://www.saucedemo.com/static/media/sl-404.168b1cce.jpg";
+        for (WebElement each: pictures){
+            Assert.assertEquals(each.getAttribute("src"), picturePath);
+        }
     }
 }
