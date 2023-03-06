d38b96c2dca7785d1155c2df5b749cca277a4dbc
ThanyaBagheri <120802259+ThanyaBagheri@users.noreply.github.com>
2023-03-06 13:49:42 -0500

    updated CartPage CartTest and Hompage also change a some code

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CartPage extends BasePage{
@@ -8,5 +10,24 @@ public class CartPage extends BasePage{
         super(driver);
         PageFactory.initElements(driver, this);
     }
-//testing
+    @FindBy(id = "user-name")
+    public WebElement usernameInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    public WebElement loginBtn;
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    public WebElement addToCartBtn;
+
+    @FindBy(id = "shopping_cart_container")
+    public WebElement cartBtn;
+
+    @FindBy(className = "inventory_item_name")
+    public WebElement item;
+
+    @FindBy(id = "remove-sauce-labs-backpack")
+    public WebElement removeFromCartBtn;
+
 }
--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,14 +1,38 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import pages.BasePage;
+import org.testng.annotations.Test;
 import pages.CartPage;
-import pages.HomePage;
 
 public class CartTest extends BaseTest {
     CartPage page;
     @BeforeMethod
     public void setUp() {
         page = new CartPage(driver);
+    }
+
+    @Test(testName = "US309: Add to cart", description = "User should be able to add an item to cart with a click of a button. Added item should be displayed in the Cart view")
+    public void test309() {
+        page.sendKeys(page.usernameInput, "standard_user");
+        page.sendKeys(page.passwordInput, "secret_sauce");
+        page.click(page.loginBtn);
+
+        page.addToCartBtn.click();
+        page.cartBtn.click();
+
+        Assert.assertTrue(page.item.isDisplayed());
+    }
+
+    @Test(testName = "US 310 - Remove from cart", description = "User should be able to remove added item from the cart with a click of a button")
+    public void test310() {
+        page.sendKeys(page.usernameInput, "standard_user");
+        page.sendKeys(page.passwordInput, "secret_sauce");
+        page.click(page.loginBtn);
+
+        page.addToCartBtn.click();
+        page.cartBtn.click();
+
+        Assert.assertTrue(page.removeFromCartBtn.isEnabled());
     }
 }
