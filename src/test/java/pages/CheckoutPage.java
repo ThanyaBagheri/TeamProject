c53e90489dc459ddf04dd72ec0a039397450aef1
ThanyaBagheri <120802259+ThanyaBagheri@users.noreply.github.com>
2023-03-07 21:18:50 -0500

    added US312

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CheckoutPage extends BasePage{
@@ -8,4 +10,28 @@ public class CheckoutPage extends BasePage{
         super(driver);
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(id = "checkout")
+    public WebElement checkoutBtn;
+
+    @FindBy(id = "first-name")
+    public WebElement firstNameFill;
+
+    @FindBy(id = "last-name")
+    public WebElement lastNameFill;
+
+    @FindBy(id = "postal-code")
+    public WebElement zipCodeFill;
+
+    @FindBy(id = "continue")
+    public WebElement continueBtn;
+
+    @FindBy(xpath = "//div[normalize-space()='Payment Information']")
+    public WebElement paymentInfo;
+
+    @FindBy(xpath = "//div[normalize-space()='Shipping Information']")
+    public WebElement shippingInfo;
+
+    @FindBy(xpath = "//div[normalize-space()='Price Total']")
+    public WebElement priceTotal;
 }
--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -1,15 +1,36 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.CartPage;
 import pages.CheckoutPage;
 import pages.LoginPage;
 
 public class CheckoutTest extends BaseTest{
     CheckoutPage page;
     LoginPage loginPage;
+    CartPage cartPage;
     @BeforeMethod
     public void setUp() {
         page = new CheckoutPage(driver);
         loginPage = new LoginPage(driver);
+    }
+
+    @Test(testName = "US 312 - Checkout Overview Info", description = "When user enters checkout information, verify headers: Payment information, Shipping information and Price Total are displayed")
+    public void test312(){
+        loginPage.login();
+        cartPage.cartBtn.click();
+        page.checkoutBtn.click();
+
+        page.firstNameFill.sendKeys("JohnDoe");
+        page.lastNameFill.sendKeys("Doe");
+        page.zipCodeFill.sendKeys("22192");
+
+        page.continueBtn.click();
+
+        Assert.assertTrue(page.paymentInfo.isDisplayed());
+        Assert.assertTrue(page.shippingInfo.isDisplayed());
+        Assert.assertTrue(page.paymentInfo.isDisplayed());
     }
 }
