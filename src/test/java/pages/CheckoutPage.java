548dc866f68a7bded0f322e595bcf1d1a2c9c1ca
Uran-Suranchiyev <uransura@gmail.com>
2023-03-08 09:14:40 -0500

    save changes

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -1,11 +1,70 @@
 package pages;
 
+import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+import java.util.Random;
 
 public class CheckoutPage extends BasePage{
     public CheckoutPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
+    @FindBy(xpath = "//*[text()='Add to cart']")
+    public List<WebElement> inventoryItemsBtns;
+    @FindBy(xpath = "//a[@class='shopping_cart_link']")
+    public WebElement shoppingCartBtn;
+    @FindBy(id = "first-name")
+    public WebElement firstNameField;
+    @FindBy(id = "last-name")
+    public WebElement lastNameField;
+    @FindBy(id = "postal-code")
+    public WebElement postalCodeField;
+
+    @FindBy(id = "checkout")
+    public WebElement checkoutBtn;
+    @FindBy(id = "continue")
+    public WebElement continueBtn;
+    @FindBy(xpath = "//div[@class='summary_subtotal_label']")
+    public WebElement itemTotal;
+    @FindBy(xpath = "//div[@class='summary_tax_label']")
+    public WebElement tax;
+    @FindBy(xpath = "//div[@class='summary_info_label summary_total_label']")
+    public WebElement total;
+
+
+
+    public void addToCartAllItems(){
+        for (WebElement each : inventoryItemsBtns){
+            click(each);
+        }
+    }
+    public void fillCheckoutForm(){
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String postalCode = faker.address().zipCode();
+
+        click(checkoutBtn);
+
+        sendKeys(firstNameField, firstName);
+        sendKeys(lastNameField, lastName);
+        sendKeys(postalCodeField, postalCode);
+
+        click(continueBtn);
+    }
+    public double getSumFromString(WebElement sumStr){
+        String str = sumStr.getText();
+        System.out.println(str);
+        String[] split = str.split(" ");
+        String strNum = split[split.length-1].substring(1);
+        System.out.println(strNum);
+        double sum = Double.parseDouble(strNum);
+        return sum;
+    }
+
 }
--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -1,8 +1,11 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.CheckoutPage;
 import pages.LoginPage;
+import utils.BrowserUtils;
 
 public class CheckoutTest extends BaseTest{
     CheckoutPage page;
@@ -12,4 +15,28 @@ public class CheckoutTest extends BaseTest{
         page = new CheckoutPage(driver);
         loginPage = new LoginPage(driver);
     }
+
+    @Test(testName = "US 313 -Checkout Overview - Price Total\n")
+    public void test313(){
+        // login
+        loginPage.login();
+        // add to cart
+        page.addToCartAllItems();
+        //switch to cart page
+        page.click(page.shoppingCartBtn);
+        //fill checkout form and click continue
+        page.fillCheckoutForm();
+        //verify
+        double item = page.getSumFromString(page.itemTotal);
+        double tax = page.getSumFromString(page.tax);
+        double total = page.getSumFromString(page.total);
+
+        Assert.assertEquals(total, item + tax);
+    }
+    @Test(testName = "US 316- Checkout: Complete - Back Home Button")
+    public void test316(){
+
+    }
+
+
 }
